2a3cf7f92bc418ec8c46e54c85e822d936ef9503
Nasibeh Mohammadi <namo@nmbu.no>
2020-01-21 19:09:39 +0100

    Add comments to statistical tests

--- tests/test_statistical.py
+++ tests/test_statistical.py
@@ -12,21 +12,20 @@ __author__ = 'Mohamed Radwan, Nasibeh Mohammadi'
 __email__ = 'mohamed.radwan@nmbu.no, nasibeh.mohammadi@nmbu.no'
 
 import pytest
-from biosim.landscapes import Desert, Ocean, Mountain, Savannah, Jungle
+from biosim.landscapes import Jungle
 from biosim.fauna import Herbivore, Carnivore
-from biosim.map import Map
 from scipy import stats
 import mock
 from mock import patch
-from random import seed
-import numpy as np
+import numpy
 
 
 class TestGaussian:
     @pytest.fixture
     def generate_animal_data(self):
         """
-        Generates population of animals and an array of their initial weight.
+        Generates population of animals and an array of their initial weight to
+        be used in test_birth_weight_dist
 
         Returns
         -------
@@ -39,10 +38,9 @@ class TestGaussian:
             carn.set_default_weight()
             birth_weight_list.append(carn.weight)
         for _ in range(20):
-            h = Herbivore()
-            h.set_default_weight()
-            z = h.weight
-            birth_weight_list.append(z)
+            herb = Herbivore()
+            herb.set_default_weight()
+            birth_weight_list.append(herb.weight)
         return birth_weight_list
 
     def test_birth_weight_dist(self, generate_animal_data):
@@ -59,10 +57,8 @@ class TestGaussian:
         birth_weight_list = generate_animal_data
         k2, p_value = stats.normaltest(birth_weight_list)
         alpha = 0.01
-        if p_value < alpha:
-            print("The null hypothesis can be rejected")
-        else:
-            print("The null hypothesis cannot be rejected")
+        assert p_value >= alpha
+        print("The null hypothesis cannot be rejected")
 
 
 class TestProbability:
@@ -71,9 +67,29 @@ class TestProbability:
     generator with fixed value.
     """
     def test_die(mocker):
+        """
+        Tests the die_animal method and death_prob in two conditions:
+        1. animal with fitness zero (herb_1).
+        2. a fixed return value for random number with the use of mock (carn_1).
+
+        Explanation of code:
+        First, generating animals population for one of the landscapes types
+        (i.e. jungle).
+        Generating herb_1 with weight 0 to get the fitness zero and gain
+        death_prob = True.
+        Also, generates carn_1 with some default value just to have fixed values
+        for its fitness.
+        Then, use mock for numpy random number and set the return_value to it in
+        the way taht it always fulfill death_prob = True for carn_1
+        Finally, run the die_animal method for all of the animals of jungle
+        instance and assert that the 2 animals are died and only the other 2 are
+        in the animal list.
+
+        """
+
         herb_1 = Herbivore(6, 0)
         herb_2 = Herbivore()
-        carn_1 = Carnivore(1,5)
+        carn_1 = Carnivore(1, 5)
         carn_2 = Carnivore()
         params = {'f_max': 10.0}
         j=Jungle(params)
@@ -92,6 +108,5 @@ class TestProbability:
         h_count = len(j.in_cell_fauna['Herbivore'])
         c_count = len(j.in_cell_fauna['Carnivore'])
         fauna_count = h_count + c_count
-        print(fauna_count)
         assert fauna_count == 2
 
